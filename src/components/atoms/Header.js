1f26a9134a6fd85425b10385785fb224b1749b31
Francis Tatepo <46090435+ciscodes@users.noreply.github.com>
2021-11-19 12:12:38 +0100

    Update Header.js

--- src/components/atoms/Header.js
+++ src/components/atoms/Header.js
@@ -3,6 +3,7 @@ import styled from 'styled-components';
 const Header = styled.header`
     display:flex;
     margin: 13px;
+    margin-bottom: 50px;
     flex-direction: row;
     justify-content: space-around;
 `;
