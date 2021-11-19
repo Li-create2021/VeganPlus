74f549a8aafe24ff3a2903feca4f8afd6581efef
Francis Tatepo <46090435+ciscodes@users.noreply.github.com>
2021-11-19 12:18:40 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ function App() {
 
     axios
 
-      .get("https://api.spoonacular.com/recipes/complexSearch?apiKey=14a6c274a3c145b7a6b898948168891f&diet=vegan&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&addRecipeNutrition=true&tags=diet=vegan&number=150&limitLicense=true", {
+      .get("https://api.spoonacular.com/recipes/complexSearch?apiKey=14a6c274a3c145b7a6b898948168891f&diet=vegan&instructionsRequired=true&fillIngredients=true&addRecipeInformation=true&addRecipeNutrition=true&tags=diet=vegan&number=50&limitLicense=true", {
         cancelToken: source.token
       })
 
